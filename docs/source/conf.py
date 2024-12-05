ac9e5d37a8c458eb26a872d58184509049fc37de
Martin E. Liza <mliza@email.arizona.edu>
2024-12-05 18:21:38 -0500

    modified static folde

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ autodoc_mock_imports = ['setup']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 latex_elements = {
     'papersize': 'a4paper',  # Set the paper size to A4 (or 'letterpaper' for US Letter)
