49a59c52abf18afbc63eb5f838038e4508459fcb
Martin E. Liza <60142782+mliza@users.noreply.github.com>
2024-12-06 13:16:56 -0500

    Update aerodynamics.py

--- haot/aerodynamics.py
+++ haot/aerodynamics.py
@@ -16,11 +16,14 @@ def sutherland_law_viscosity(temperature_K: float, molecule: str = "Air") -> flo
     Calculates the Sutherland's law of viscosity
 
     Parameters:
-        temperature_K: reference temperature
+        temperature_K: reference temperature in [K]
         molecule: Air (default), Argon, N2, O2
 
     Returns:
         dynamic viscosity in [kg/ms]
+
+    Examples:
+        >> sutherland_law_viscosity(300.0)
 
     """
     const = constants_tables.sutherland_constants(molecule)
@@ -37,11 +40,14 @@ def sutherland_law_conductivity(temperature_K: float, molecule: str = "Air") -> 
     Calculates the Sutherland's law of thermal conductivity
 
     Parameters:
-        temperature_K: reference temperature
+        temperature_K: reference temperature in [K]
         molecule: Air (default), Argon, N2, O2
 
     Returns:
         thermal conductivity in [W/mK]
+
+    Examples:
+        >> sutherland_law_conductivity(300.0)
 
     """
     const = constants_tables.sutherland_constants(molecule)
@@ -54,9 +60,23 @@ def sutherland_law_conductivity(temperature_K: float, molecule: str = "Air") -> 
     return const["conductivity_ref"] * thermal_conductivity  # [W/mK]
 
 
-def air_atomic_molar_mass():
-    """Returns the atomic molar mass of an 11-species air in [g/mol]"""
-    molecules = ["N+", "O+", "NO+", "N2+", "O2+", "N", "O", "NO", "N2", "O2"]
+def air_atomic_molar_mass(molecules: str = None) -> dict:
+    """
+    Returns the atomic molar mass
+
+    Paremeters:
+        molecule: Molecules that need the molar mass (11 species air is the default)
+
+    Returns:
+        species in [g/mol]
+
+    Examples:
+        >> air_atomic_molar_mass()
+
+    """
+    if not molecule:
+        molecules = ["N+", "O+", "NO+", "N2+", "O2+", "N", "O", "NO", "N2", "O2"]
+        
     air_atomic_dict = {i: molmass.Formula(i).mass for i in molecules}
     
     return air_atomic_dict  # [g/mol]
@@ -67,15 +87,19 @@ def speed_of_sound(temperature_K: float, adiabatic_indx: float = 1.4) -> float:
     Calculates the speed of sound
 
     Parameters:
-        temperature_K: reference temperature
+        temperature_K: reference temperature in [K]
         adiabatic_indx: adiabatic index, 1.4 (default)
 
     Returns:
         speed of sound in [m/s]
 
+    Examples:
+        speed_of_sound(
+
     """
     gas_const = s_consts.R  # [J/mol*K]
-    air_atomic_mass = air_atomic_molar_mass()  # [g/mol]
+    air_atomic_mass = air_atomic_molar_mass(["N2", "O2", "CO2"])  # [g/mol]
+
     air_molecular_mass = (
         0.7803 * air_atomic_mass["N2"]  # [kg/mol]
         + 0.2099 * air_atomic_mass["O2"]
